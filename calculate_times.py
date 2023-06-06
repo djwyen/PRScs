f526ba10409e3d15b33e631b21ddf1669c262b9f
Derek Yen <mit.dereky@gmail.com>
2023-06-06 18:10:39 -0400

    Write in script to calculate stats and plot them from

--- calculate_times.py
+++ calculate_times.py
@@ -1,75 +1,146 @@
 """
-Quick script to calculate the time blowup for CGM based sampling (in plaintext) over the vanilla method.
-Preliminary numbers when evaluated on the test dataset provided in the PRScs repo:
-    Vanilla MVN sampling took, on average, 0.0023184492500000022 seconds
-    CGM-based MVN sampling took, on average, 0.013257944250000023 seconds
-    The blowup is about a factor of 5.718453509387799
----
-Vanilla block sizes to times: {135: 0.003336923999999999, 176: 0.0017466090000000014, 511: 0.005111019000000006, 178: 0.0014734180000000004}
-CGM block sizes to times: {135: 0.005527726999999996, 176: 0.0038376510000000005, 511: 0.024762743000000014, 178: 0.003913789000000005}
-On blocksize 135, the factor is 1.6565336819178376
-On blocksize 176, the factor is 2.1972009762917732
-On blocksize 511, the factor is 4.844971814818138
-On blocksize 178, the factor is 2.6562652281972965
+Quick script to calculate the times/avg iterations recorded for each sample from PRScs, put them into a dataframe to be workable, and plot them.
 """
+import csv
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
+LOGFILE = 'june6.log'
+CSV_DIR = 'mvn_csvs/'
+VANILLA_CSV = 'vanilla.csv'
 
-VANILLA_LOG = './vanilla.log'
-CGM_LOG = './cgm_mvn.log'
+def calculate_stats(filepath):
+    # maps a block number to its block size
+    blkno_to_blocksize = {}
+    # maps a block number to a list of times it took to sample an MVN for that block
+    blkno_to_times = {}
+    # maps a block number to a list of the number of iterations it took to sample an MVN for that block
+    blkno_to_n_iterations = {}
+
+    with open(filepath) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for entry in reader:
+            mcmc_iteration, block_number, block_size, sampling_time, n_iterations = entry
+            mcmc_iteration = int(mcmc_iteration)
+            block_number = int(block_number)
+            block_size = int(block_size)
+            sampling_time = float(sampling_time)
+            n_iterations = int(n_iterations) if n_iterations != '' else None
+
+            if block_number not in blkno_to_blocksize:
+                # we assume each will be updated in parallel
+                blkno_to_blocksize[block_number] = block_size
+                blkno_to_times[block_number] = []
+                blkno_to_n_iterations[block_number] = []
+            
+            blkno_to_times[block_number].append(sampling_time)
+            if n_iterations is not None:
+                blkno_to_n_iterations[block_number].append(n_iterations)
+    
+    blkno_to_avgtime = {blkno: sum(times) / len(times) for blkno, times in blkno_to_times.items()}
+    if n_iterations is not None:
+        blkno_to_avgiters = {blkno: sum(iters) / len(iters) for blkno, iters in blkno_to_n_iterations.items()}
+    else:
+        blkno_to_avgiters = None
+
+    # TODO could also calculate the stdev on the times, iters and return that too for more info.
+    # the CGM alg is deterministic, but the particular eta we use is probabilistic due to containing an MVN sample, and also the Psi inv changes each mcmc iteration.
+    return blkno_to_blocksize, blkno_to_avgtime, blkno_to_avgiters
 
 def main():
-    van_size_to_times = {}
-    cgm_size_to_times = {}
 
-    with open(VANILLA_LOG) as van_f, open(CGM_LOG) as cgm_f:
-        for van_line, cgm_line in zip(van_f, cgm_f):
-            
-            if "MVN sampling" in van_line and "MVN sampling" in cgm_line:
-                van_blksize, van_time = process_line(van_line)
-                if van_blksize not in van_size_to_times:
-                    van_size_to_times[van_blksize] = []
-                van_size_to_times[van_blksize].append(van_time)
+    blocksizes = []
+    vanilla_blocksize_to_avgtime = {}
 
-                cgm_blksize, cgm_time = process_line(cgm_line)
-                if cgm_blksize not in cgm_size_to_times:
-                    cgm_size_to_times[cgm_blksize] = []
-                cgm_size_to_times[cgm_blksize].append(cgm_time)
-                assert van_blksize == cgm_blksize
+    # maps a particular error rate (under a particular algorithm) to a list of values, which are in the listed order of the blocksizes
+    # so for example, the entry '0.1' : [5, 10, 3] with blocksizes = [30, 50, 20] means that, at error rate 0.1, on blocksize 30 it has value 5, on blocksize 50 it has value 10, and on blocksize 20 it has value 3
+    # this format is convenient for later creating a pandas dataframe
+    precond_err_to_avgtimes = {}
+    nonprecond_err_to_avgtimes = {}
+
+    precond_err_to_avgiters = {}
+    nonprecond_err_to_avgiters = {}
+
+    # we will load up and scan the vanilla csv separately since it contains some crucial info we use later
+    blkno_to_blocksize, blkno_to_avgtime, _ = calculate_stats(os.path.join(CSV_DIR, VANILLA_CSV))
+    for blkno in range(len(blkno_to_blocksize)):
+        blocksize = blkno_to_blocksize[blkno]
+        blocksizes.append(blocksize)
+        vanilla_blocksize_to_avgtime[blocksize] = blkno_to_avgtime[blkno]
+
+
+    for filename in os.listdir(CSV_DIR):
+        if 'vanilla' in filename:
+            # we loaded up vanilla separately
+            continue
+        assert 'cgm' in filename
+        # infer the properties of this file from the name
+        samplertype = ''
+        error_tolerance = None
+        samplertype = filename.split('_')[0] # `preconditioned` or `nonpreconditioned`
+        ending = filename.split('_')[-1]
+        if ending != 'cgm.csv': # i.e. this run was with a specified error tolerance
+            error_tolerance = float(ending.split('.csv')[0])
+        else:
+            error_tolerance = 0.0
+
+        blkno_to_blocksize, blkno_to_avgtime, blkno_to_avgiters = calculate_stats(os.path.join(CSV_DIR, filename))
+        
+        if samplertype == 'preconditioned':
+            precond_err_to_avgtimes[error_tolerance] = [blkno_to_avgtime[blkno] for blkno in range(len(blkno_to_blocksize))]
+            precond_err_to_avgiters[error_tolerance] = [blkno_to_avgiters[blkno] for blkno in range(len(blkno_to_blocksize))]
+        else:
+            nonprecond_err_to_avgtimes[error_tolerance] = [blkno_to_avgtime[blkno] for blkno in range(len(blkno_to_blocksize))]
+            nonprecond_err_to_avgiters[error_tolerance] = [blkno_to_avgiters[blkno] for blkno in range(len(blkno_to_blocksize))]
+
     
-    van_size_to_avg_time = {size: (sum(times) / len(times)) for size, times in van_size_to_times.items()}
-    cgm_size_to_avg_time = {size: (sum(times) / len(times)) for size, times in cgm_size_to_times.items()}
-    print("Vanilla block sizes to times:", van_size_to_avg_time)
-    print("CGM block sizes to times:", cgm_size_to_avg_time)
+    # for the CGM methods, desired pandas dataframes
+    # print(precond_err_to_avgtimes)
+    
+    precond_avgtime = pd.DataFrame(data=precond_err_to_avgtimes, index=blocksizes)
+    precond_avgiters = pd.DataFrame(data=precond_err_to_avgiters, index=blocksizes)
+    nonprecond_avgtime = pd.DataFrame(data=nonprecond_err_to_avgtimes, index=blocksizes)
+    nonprecond_avgiters = pd.DataFrame(data=nonprecond_err_to_avgiters, index=blocksizes)
+    
+    # since blocks were appended in the order of their block number, they aren't sorted on block size; so we sort them here
+    def sort_dataframe(df):
+        df = df.sort_index()
+        return df.reindex(sorted(df.columns), axis=1)
 
-    for size, vantime in van_size_to_avg_time.items():
-        ratio = cgm_size_to_avg_time[size] / vantime
-        print(f"On blocksize {size}, the factor is", ratio)
+    precond_avgtime = sort_dataframe(precond_avgtime)
+    precond_avgiters = sort_dataframe(precond_avgiters)
+    nonprecond_avgtime = sort_dataframe(nonprecond_avgtime)
+    nonprecond_avgiters = sort_dataframe(nonprecond_avgiters)
+
+    # data is now in a very convenient form for plotting
+    # TODO choose colors non arbitrarily to help visually group by alg type
+    plt.plot(blocksizes, [vanilla_blocksize_to_avgtime[blocksize] for blocksize in blocksizes], '.-', label='Cholesky')
+    plt.plot(precond_avgtime.index, precond_avgtime[0.0], '.-', label='exact preconditioned CGM')
+    plt.plot(nonprecond_avgtime.index, nonprecond_avgtime[0.0], '.-', label='exact nonpreconditioned CGM')
+    plt.plot(precond_avgtime.index, precond_avgtime[1e-5], '.-', label='1e-5 error precond')
+    plt.plot(nonprecond_avgtime.index, nonprecond_avgtime[1e-5], '.-', label='1e-5 error nonprecond')
+    plt.plot(precond_avgtime.index, precond_avgtime[1e-3], '.-', label='1e-3 error precond')
+    plt.plot(nonprecond_avgtime.index, nonprecond_avgtime[1e-3], '.-', label='1e-3 error nonprecond')
+
+    # TODO break the x axis potentially?
+    # plt.xticks(blocksizes)
+    plt.gca().set_xticks(blocksizes, minor=True)
+    plt.legend()
+    plt.title('Average sampling time by blocksize')
+    plt.show()
 
 
-def process_line(line):
-    # really scuffed extraction of relevant quantities from the log
-    time = float(line.split('took ')[1].split(' seconds')[0])
-    blocksize = int(line.split('size ')[1].split(' took')[0])
-    return blocksize, time
-
-def old_main():
-    vanilla_avg = average_time(VANILLA_LOG)
-    cgm_avg = average_time(CGM_LOG)
-    print("Vanilla MVN sampling took, on average,", vanilla_avg)
-    print("CGM-based MVN sampling took, on average,", cgm_avg)
-    print("The blowup is about", cgm_avg/vanilla_avg)
-
-def average_time(filepath):
-    total_time = 0
-    count = 0
-    with open(filepath) as f:
-        for line in f:
-            if "MVN sampling" in line:
-                count += 1
-                # really scuffed number extraction
-                time = float(line.split('took ')[1].split(' seconds')[0])
-                total_time += time
-    return total_time / count
+    for err in [1e-3, 1e-5, 1e-7]:
+        label = 'exact' if err == 0.0 else str(err) # well, it's not that sensible to think about this as exact obviously is a straight line?
+        plt.plot(precond_avgiters.index, precond_avgiters[err], '.--', label = label + ' precond')
+        plt.plot(nonprecond_avgiters.index, nonprecond_avgiters[err], '.--', label = label + ' nonprecond')
+    plt.plot(precond_avgiters.index, precond_avgiters[0.0], label='exact') # exact sampling is the max number of iterations regardless of whether we precondition
+    plt.gca().set_xticks(blocksizes, minor=True)
+    plt.legend()
+    plt.title('Average number of iterations by blocksize')
+    plt.show()
 
 
 if __name__ == '__main__':
