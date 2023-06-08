3609bd7516523e109836588e63cb3e27e84e7be8
Derek Yen <mit.dereky@gmail.com>
2023-06-07 23:00:48 -0400

    Add automated testing files

---
+++ test_all_samplers.zsh
@@ -0,0 +1,70 @@
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=False --log_file=june7mvntrials --mvn_dir=mvn_csvs
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.1
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.1
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.01
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.01
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.000001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.000001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.5
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.5
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.05
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.05
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00005
+
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0000001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.0000001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00000005
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=True --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00000001
+
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --out_dir=output/eur --use_cgm=Precond --log_file=june7mvntrials --mvn_dir=mvn_csvs --err_tol=0.00000001
