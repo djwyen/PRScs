6fd1331220a9f75d5349443d0955783ef8d34ff9
Derek Yen <mit.dereky@gmail.com>
2023-08-03 15:14:16 -0400

    Add trials for benchmarking

---
+++ run_aug3_trials.zsh
@@ -0,0 +1,1 @@
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --trials_file=aug3_trials.csv --n_iter=1000 --n_burnin=500 --thin=5 --out_dir=output/aug3benchmarking --log_file=logs/aug3benchmarking
