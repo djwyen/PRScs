e8e31a4728fba36bd6bd67a7b43c6b78f0dab895
Derek Yen <mit.dereky@gmail.com>
2023-08-11 15:43:12 -0400

    Add scripts for additional trials

---
+++ run_aug10_trials.zsh
@@ -0,0 +1,2 @@
+python PRScs.py --ref_dir=data/ldblk_1kg_eur --bim_prefix=test_data/test --sst_file=test_data/sumstats.txt --n_gwas=200000 --chrom=22 --phi=1e-2 --trials_file=aug10_trials.csv --n_iter=1000 --n_burnin=500 --thin=5 --out_dir=output/aug10benchmarking --log_file=logs/aug10benchmarking
+ 
